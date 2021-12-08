aa781d89c601a2b598b065bdd950fa645d54a795
SyedOsamaBinAzhar <osamaazhar.edu@gmail.com>
2021-12-08 18:48:34 +0500

    Image cropped using antdesign. Cropped image is drawn on Canvas and then filter is applied

--- src/components/UploadBtn/UploadBtn.jsx
+++ src/components/UploadBtn/UploadBtn.jsx
@@ -1,12 +1,67 @@
-import React from 'react'
-import { Upload, message, Button } from 'antd';
-import { UploadOutlined } from '@ant-design/icons';
+import React, { useEffect, useState } from 'react'
+import { Upload } from 'antd';
+import ImgCrop from 'antd-img-crop';
+import fabric from 'fabric'
 
-const UploadBtn = () => {
+const UploadBtn = ({setCroppedImg}) => {
+
+    const [croppedLocalImg,setCroppedLocalImg] = useState("");
+
+    const [fileList, setFileList] = useState([
+        {
+          uid: '-1',
+          name: 'image.png',
+          status: 'done',
+          url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+        }
+      ]);
+    
+    
+    const onChange = ({ fileList: newFileList }) => {
+        setFileList(newFileList);
+      };
+
+
+      const onPreview = async file => {
+          console.log("on prev")
+        //   console.log(file.url)
+        let src = file.url;
+        if (!src) {
+          src = await new Promise(resolve => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file.originFileObj);
+            reader.onload = () => resolve(reader.result);
+          });
+        }
+        // console.log(src)
+        const image = new Image();
+        image.src = src;
+        setCroppedLocalImg(src)
+        
+      };
+
+      useEffect(() => {
+        setCroppedLocalImg(croppedLocalImg)
+        setCroppedImg(croppedLocalImg)
+      }, [croppedLocalImg])
+
     return (
-        <Upload>
-    <Button icon={<UploadOutlined />}>Click to Upload</Button>
-  </Upload>
+      <>
+      <ImgCrop rotate>
+      <Upload
+        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+        listType="picture-card"
+        // fileList={fileList}
+        onChange={(e) => onChange(e)}
+        onPreview={onPreview}
+      >
+        {fileList.length < 5 && '+ Upload'}
+      </Upload>
+    </ImgCrop>
+    <div style = {{height :"50vh",width:"30vw",background:"orange"}}>
+        <img src={croppedLocalImg} style = {{height :"45vh",width:"25vw",background:"pink"}} id = "img"></img>
+    </div>
+      </>
     )
 }
 
--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,16 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Layout.css"
 import UploadBtn from "../components/UploadBtn/UploadBtn"
 import { Row, Col } from 'antd';
+import { fabric } from 'fabric';
 
 const Layout = () => {
+
+    const [croppedImg,setCroppedImg] = useState("");
+
+    useEffect(() => {
+        if(croppedImg){
+            console.log(croppedImg)
+            var canvas = new fabric.Canvas('c');
+            console.log(canvas)
+            fabric.Image.fromURL(croppedImg, function(oImg) {
+                // canvas.add(oImg);
+                oImg.scale(0.1);
+                oImg.filters.push(new fabric.Image.filters.Grayscale());
+                oImg.applyFilters();
+                // add image onto canvas (it also re-render the canvas)
+                canvas.add(oImg);
+                // canvas.add(oImg);
+                
+              });
+        }
+    }, [croppedImg])
+
     return (
         <>
         <Row className="root">
             <Col span={12} className="col1">
-                <UploadBtn/>
+                <UploadBtn setCroppedImg = {setCroppedImg}/>
             </Col> 
-            <Col span={12} className="col2">col-2</Col>
+
+            <Col span={12} className="col2">
+            <div style = {{height:500,width:500,background:"pink",position:"relative"}}>
+            <canvas id="c" style={{border:"1px solid black",position:"absolute"}}></canvas>
+            </div>
+            {/* <img src={croppedImg} id="my-image" height = {400} width = {400}/> */}
+            </Col>
         </Row>            
         </>
     )
